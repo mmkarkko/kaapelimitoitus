a4350402a2f84f090bbe31f5c2aad505bdc5ff10
mmkarkko <miia.arkko@gmail.com>
2023-03-08 20:34:33 +0200

    taulukko jonkinlainen tulostus

--- src/mitoitus/Kaapelimitoitus.java
+++ src/mitoitus/Kaapelimitoitus.java
@@ -3,6 +3,9 @@
  */
 package mitoitus;
 
+import java.io.PrintStream;
+import java.util.Arrays;
+
 /**
  * Ohjelma kaapelin poikkipinta-alan mitoittamiseksi.
  * 
@@ -11,25 +14,32 @@ package mitoitus;
  *
  */
 public class Kaapelimitoitus {
-//    private static double[][] taulukkoA;
+    
+    /**
+     * Tulostaa
+     * @param ps Tietovirta, johon tulostetaan
+     * @param matriisi joka tulostetaan
+     */
+    public static void tulosta(PrintStream ps, String[][] matriisi) {
+        ps.println(Arrays.deepToString(matriisi));
+    }
+            
+        
+    
     
     
     @Override
     public String toString() {
-        
+        return "";
     }
+    
 
     /**
      * Ohjelma kaapelimitoituksen testaamiseksi
      * @param args ei käytössä
      */
-    public static void main(String[] args) {
-        
-//        int rivit     = 17;
-//        int sarakkeet = 6;
-//        
-//        taulukkoA = new double[rivit][sarakkeet];
-        
+    public static void main(String[] args) {     
+       
      // TODO: tarkista, mitä tehdään moniarvoisten kanssa. Nyt korvattu 0:lla
         double[][] taulukko = {
                 {1.5, 1.5, 1.5, 1.5, 1.5, 1.5}, // rivi 1 --> sulake  6
@@ -51,19 +61,30 @@ public class Kaapelimitoitus {
                 {240, 240, 300,   0,   0,   0}, // rivi 18 --> sulake 250  // TODO: tämän rivin nollat ovat kirjan taulukossa viivoja!!
         };
         
-        // Täytetään taulukko 0:lla
-//        int alustusArvo = 0;
-//        int laskuri =0 ;   
-//        for (int i = 0; i < rivit; i++) {
-//            int sar = taulukkoA[i].length;
-//            for (int j = 0; j < sar; j++) {
-//                taulukkoA[i][j] = alustusArvo;
-//                laskuri++;
-//                System.out.println(taulukkoA.toString() + " " + laskuri);
-//            }
-//        }
+        String[][] t2 = {
+                {"1.5", "1.52", "1.5", "1.5", "1.5", "1.5"}, // rivi 1 --> sulake  6
+                {"1.5", "1.5",   "0",   "0",   "0",   "0"}, // rivi 2 --> sulake 10
+                {"1.5", "1.5", "2.5", "2.5", "2.5",  "4"}, // rivi 3 --> sulake 13^4
+                {"2.5", "2.5",   "0",   "4",   "0",   "6"}, // rivi 4 --> sulake 16
+                {  "0",   "4",   "0",   "6",   "0",  "10"}, // rivi 5 --> sulake 20
+                {  "0",   "6",   "0",  "10",  "10",  "16"}, // rivi 7  --> sulake 25
+                {  "0",  "10",  "10",   "0",  "16",   "0"}, // rivi 8  --> sulake 32
+                { "10",  "10",   "0",  "16",   "0",  "25"}, // rivi 9  --> sulake 35
+                { "10",   "0",  "16",  "16",  "25",   "0"}, // rivi 10 --> sulake 40
+                { "16",  "16",  "25",  "25",   "0",   "0"}, // rivi 11 --> sulake 50
+                { "25",  "25",   "0",  "35",  "50",   "0"}, // rivi 12 --> sulake 63
+                { "35",  "35",  "50",   "0",  "70",  "95"}, // rivi 13 --> sulake 80
+                { "50",   "0",  "70",   "0",  "95",   "0"}, // rivi 14 --> sulake 100
+                { "70",   "0",  "95",   "0",   "0", "185"}, // rivi 15 --> sulake 125
+                {"120", "120", "150", "185", "240", "300"}, // rivi 16 --> sulake 160
+                {"150", "185", "240", "300",   "-",   "-"}, // rivi 17 --> sulake 200
+                {"240", "240", "300",   "-",   "-",   "-"}, // rivi 18 --> sulake 250
+        };
         
         System.out.println(taulukko.toString());
+        System.out.println();
+        tulosta(System.out, t2);
+
     
     }
 
