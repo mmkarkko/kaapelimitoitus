40cebc5f7f01726f034a0ac63e38a24ac9bb0bc7
mmkarkko <miia.arkko@gmail.com>
2023-03-09 14:41:13 +0200

    haearvo testit kirjoitettu

--- src/mitoitus/Kaapelimitoitus.java
+++ src/mitoitus/Kaapelimitoitus.java
@@ -15,9 +15,9 @@ import java.util.Arrays;
  */
 public class Kaapelimitoitus {
     
-    private char      asennustapa;
-    private static double korjausKerroin;
-    private static int            sulake;
+    private static char      asennustapa        = 'A';
+    private static double korjausKerroin = 0.9;
+    private static int            sulake =  10;
 
     
     /**
@@ -42,6 +42,7 @@ public class Kaapelimitoitus {
         return rivi;
     }
   
+    
     /**
      * Tulostaa taulukon sisällön
      * @param ps Tietovirta, johon tulostetaan
@@ -56,39 +57,13 @@ public class Kaapelimitoitus {
             ps.println();
         }
     }
-//            
-// 
-//    @Override
-//    public String toString() {
-//        return "";
-//    }
-    
+
 
     /**
      * Ohjelma kaapelimitoituksen testaamiseksi
      * @param args ei käytössä
      */
     public static void main(String[] args) {     
-       
-//        double[][] taulukko = {
-//                {1.5, 1.5, 1.5, 1.5, 1.5, 1.5}, // rivi 1 --> sulake  6
-//                {1.5, 1.5,   0,   0,   0,   0}, // rivi 2 --> sulake 10
-//                {1.5, 1.5, 2.5, 2.5, 2.5,   4}, // rivi 3 --> sulake 13^4
-//                {2.5, 2,5,   0,   4,   0,   6}, // rivi 4 --> sulake 16
-//                {  0,   4,   0,   6,   0,  10}, // rivi 5 --> sulake 20
-//                {  0,   6,   0,  10,  10,  16}, // rivi 7  --> sulake 25
-//                {  0,  10,  10,   0,  16,   0}, // rivi 8  --> sulake 32
-//                { 10,  10,   0,  16,   0,  25}, // rivi 9  --> sulake 35
-//                { 10,   0,  16,  16,  25,   0}, // rivi 10 --> sulake 40
-//                { 16,  16,  25,  25,   0,   0}, // rivi 11 --> sulake 50
-//                { 25,  25,   0,  35,  50,   0}, // rivi 12 --> sulake 63
-//                { 35,  35,  50,   0,  70,  95}, // rivi 13 --> sulake 80
-//                { 50,   0,  70,   0,  95,   0}, // rivi 14 --> sulake 100
-//                { 70,   0,  95,   0,   0, 185}, // rivi 15 --> sulake 125
-//                {120, 120, 150, 185, 240, 300}, // rivi 16 --> sulake 160
-//                {150, 185, 240, 300,   0,   0}, // rivi 17 --> sulake 200  // TODO: tämän rivin nollat ovat kirjan taulukossa viivoja!!
-//                {240, 240, 300,   0,   0,   0}, // rivi 18 --> sulake 250  // TODO: tämän rivin nollat ovat kirjan taulukossa viivoja!!
-//        };
         
         String[][] taulukkoA = {
                 {  "1.5",  "1.52",     "1.5",     "1.5",     "1.5",     "1.5"}, // rivi 1 --> sulake  6
@@ -114,16 +89,47 @@ public class Kaapelimitoitus {
         
         int sarake = haeSarake();
         int rivi   = haeRivi();
-        //String arvo = haeArvo(taulukkoA, rivi, sarake);
+        String arvo = haeArvo(taulukkoA, rivi, sarake);
         
-        //System.out.println("Valittu asennustapa: A");
+        System.out.println();
+        System.out.println("Valittu asennustapa: " + asennustapa);
         System.out.println("Valittu sulake on " + sulake);
         System.out.println("Valittu korjauskerroin on " + korjausKerroin);
-        System.out.println("Haettu arvo riviltä " + rivi + ", sarakkeesta " + sarake + "taulukosta oli " );
+        System.out.println();
+        System.out.println("Haettu arvo riviltä " + rivi + ", sarakkeesta " + sarake + " taulukosta oli " + arvo);
     
     }
 
 
-
+    /**
+     * Hakee moniulotteisesta taulukosta tietyn alkion sisällön
+     * @param mat matriisi, josta alkion arvo haetaan
+     * @param rivi jolta arvo halutaan
+     * @param sarake jolta arvo halutaan
+     * @return halutun alkion sisältämä arvo
+     * @example
+     * <pre name="test">
+     *  String[][] t  = {{"1","2","3","4"},{"2","0","6","1"}};
+     *  String[][] t2 = {{"-2", "koira", "4/10", ""},{"!", "2203-34", "1.5", "-"},{"V", "352", " ", "25/16"}};
+     *  haeArvo(t, 2, 3)    === "6";
+     *  haeArvo(t, 0, 0)    === "0";
+     *  haeArvo(null, 2,4)  === "Taulukko ei kelpaa";
+     *  haeArvo(t2, 3, 4)   === "25/16";
+     *  haeArvo(t2, 1, 1)   === "-2";
+     *  haeArvo(t2, 2, 4)   === "-";
+     *  haeArvo(t2, 3, 3)   === " ";
+     *  haeArvo(t2, 1, 4)   === "";
+     *  haeArvo(t2, 9, 9)   === "Ei löydy";
+     *  haeArvo(t2, -1,-1)  === "Rivi tai sarake ei kelpaa";
+     * </pre>
+     */
+    public static String haeArvo(String[][] mat, int rivi, int sarake) {
+        String oletus = "oletus";
+        if (mat == null) return "Taulukko ei kelpaa";
+        if (mat.length < rivi) return "Ei löydy";
+        if (rivi < 0 || sarake < 0) return "Rivi tai sarake ei kelpaa";
+        
+        return oletus;
+    }
 
 }
